0f24989303afd2f77164086e392e97834102bd76
Elite Computer Tech <elitecomputer.code@gmail.com>
2025-08-22 13:45:19 +0100

    Fix: Remove duplicate slick-carousel imports in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,6 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
